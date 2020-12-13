9abf54eb679ef9cb4d6d7dba425d7829d5b78c8d
fritzel56 <20907117+fritzel56@users.noreply.github.com>
2020-12-12 22:29:01 -0500

    Update site-tracker.py
    Removed the email functions (as they now live in email_helpers). Restructure the code to compose the emails into several shorter functions.
    Removed the many print statements (was spamming the logs while adding minimal value). Just replaced those with two statements which either say success or error).
    Fixed the description for the site_url funciton (was previously copied over from hot-potatoes and miscellaneously referenced Yahoo Finance).

--- site-tracker.py
+++ site-tracker.py
@@ -7,59 +7,18 @@ import traceback
 from google.cloud import bigquery
 import os
 import pandas as pd
-from mailjet_rest import Client
+import email_helpers as eh
 import google_helpers as gh
 
 
-def error_composition():
-    """Composes an email in the event of an exception with exception details.
-    Returns:
-        dict: data structure containing the composed email ready for MJ's API
-    """
-    contact_email = os.environ['contact_email']
-    contact_name = os.environ['contact_name']
-    err = sys.exc_info()
-    err_message = traceback.format_exception(*err)
-    data = {
-        'Messages': [
-            {
-                "From": {
-                    "Email": contact_email,
-                    "Name": contact_name
-                },
-                "To": [
-                    {
-                        "Email": contact_email,
-                        "Name": contact_name
-                    }
-                ],
-                "Subject": "There was an error with the yurt tracker run",
-                "HTMLPart": "There was an error with the yurt tracker run: {} ".format(err_message),
-            }
-        ]
-    }
-    return data
-
-
-def send_email(email):
-    """Takes in a composed email and sends it using the mailjet api
-
-    Args:
-        email (dict): dict containing all relevant fields needed by the mailjet API
-    """
-    api_key = os.environ['api_key']
-    api_secret = os.environ['api_secret']
-    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
-    result = mailjet.send.create(data=email)
-    print(result)
-
-
 def site_url(start_date, end_date, resourceId):
-    """Takes in a stock ticker and returns the relevant Yahoo Finance link.
+    """Takes in a dates and resourceID and returns a URL to query for availability.
+
     Args:
         start_date (datetime): the first date we want availability results for
         end_date (datetime): the last date we want availability data for
         resourceId (str): the number associated with the site of interest
+
     Returns:
         str: the URL which will return the availability of a specific site
     """
@@ -83,35 +42,6 @@ def get_result(start_date, end_date, resourceId):
     content = urllib2.urlopen(url).read().decode('utf-8')
     content = content.replace('null', '-99')
     return json.loads(content)
-
-
-def compose_summary_email(df):
-    contact_email = os.environ['contact_email']
-    contact_name = os.environ['contact_name']
-    body = ''
-    df.sort_values(by=['date'], inplace=True)
-    for site in df.site.unique():
-        body = body+site+' is available on these dates:<br /><br />{}<br /><br /><br />'.format(df.loc[df.site == site].to_html(columns=['date_str'], header=False, index=False))
-    body = body.replace('&lt;', '<').replace('&gt;', '>')
-    data = {
-        'Messages': [
-            {
-                "From": {
-                    "Email": contact_email,
-                    "Name": contact_name
-                },
-                "To": [
-                    {
-                        "Email": contact_email,
-                        "Name": contact_name
-                    }
-                ],
-                "Subject": "Current Yurt Availability",
-                "HTMLPart": body,
-            }
-        ]
-    }
-    return data
 
 
 def kickoff():
@@ -147,28 +77,58 @@ def kickoff():
     df_last_data = gh.get_bq_data(sql, client)
     df_merged = df_last_data.merge(df, left_on=['yurt_name', 'availability_date'], right_on=['site', 'date'])
     # newly available
-    available = df_merged.loc[(df_merged.availability_x != 0) & (df_merged.availability_y == 0)]
-    for site in available.site.unique():
-        print('{} is newly available on:'.format(site))
-        print(available.loc[available.site == site, 'date'].to_list())
-    # newly booked
+    newly_available = df_merged.loc[(df_merged.availability_x != 0) & (df_merged.availability_y == 0)]
     booked = df_merged.loc[(df_merged.availability_x != 1) & (df_merged.availability_y == 1)]
-    for site in booked.site.unique():
-        print('{} is newly booked on:'.format(site))
-        print(booked.loc[booked.site == site, 'date'].to_list())
-    if (len(booked)>0) | (len(available)>0):
+    if (len(booked)>0) | (len(newly_available)>0):
         gh.write_to_gbq(df, client, site_data_table)
-    same = df_merged.loc[(df_merged.availability_x == df_merged.availability_y)]
-    for site in same.site.unique():
-        print('{} is the same on:'.format(site))
-        print(same.loc[same.site == site, 'date'].to_list())
-    currently_available = df_merged.loc[df_merged.availability_y == 0]
-    currently_available['date_str']=currently_available.date.apply(str)
-    if len(available) > 0:
-        mask = (currently_available.availability_x != 0) & (df_merged.availability_y == 0)
-        currently_available.loc[mask, 'date_str'] = '<span style="background-color: #BFFF00">'+currently_available.loc[mask, 'date_str']+'</span>'
-        email = compose_summary_email(currently_available)
-        send_email(email)
+    if len(newly_available) > 0:
+        email = compose_summary_email(newly_available, df_merged)
+        eh.send_email(email)
+
+
+def summary_email_body(all_available):
+    body = ''
+    body_base = '{} is available on these dates:<br /><br />{}<br /><br /><br />'
+    for site in all_available.site.unique():
+        site_df = all_available.loc[all_available.site == site]
+        table = site_df.to_html(columns=['dt_str'], header=False,
+                                index=False)
+        body = body + body_base.format(site, table)
+    body = body.replace('&lt;', '<').replace('&gt;', '>')
+    return body
+
+
+def compose_summary_email(newly_available, df_merged):
+    # create a table with all currently available days
+    all_available = df_merged.loc[df_merged.availability_y==0].copy(deep=True)
+    all_available['dt_str'] = all_available.date.apply(str)
+    # add HTML code to highlight the newly available days
+    mask = ((all_available.availability_x != 0) &
+            (df_merged.availability_y == 0))
+    hls = '<span style="background-color: #BFFF00">'
+    hle = '</span>'
+    all_available.loc[mask, 'dt_str'] = hls + all_available.loc[mask, 'dt_str'] + hle
+    all_available.sort_values(by=['date'], inplace=True)
+    # Compose the actual text for the body of the email
+    body = summary_email_body(all_available)
+    subject = "Current Yurt Availability"
+    # Compose the email
+    email = eh.email_composition(os.environ['contact_email'],
+                                    os.environ['contact_name'],
+                                    subject, body)
+    return email
+
+
+def error_email_body():
+    """Composes the body of the email in the event of an error with a run.
+
+    Returns:
+        str: The errors which caused the job to fail.
+    """
+    err = sys.exc_info()
+    err_message = traceback.format_exception(*err)
+    body = "There was an error with a yurt tracker run: {}".format(err_message)
+    return body
 
 
 def main(request):
@@ -180,7 +140,13 @@ def main(request):
     try:
         # try to run the main body of code
         kickoff()
+        print('Successful run.')
     except Exception:
         # if it fails. capture the exception and send out a summary email.
-        email = error_composition()
-        send_email(email)
+        print('There was an error with the yurt tracker run.')
+        subject = "There was an error with the yurt tracker run"
+        body = error_email_body()
+        email = eh.email_composition(os.environ['contact_email'],
+                                     os.environ['contact_name'],
+                                     subject, body)
+        eh.send_email(email)
